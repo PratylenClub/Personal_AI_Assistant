918e480da45810386245254fcd99cd0964f1ef00
SergioPeignier <sergio.peignier.zapata@gmail.com>
2017-05-22 22:29:31 +0200

    tests in synthesizer and wikipedia library

---
+++ tests_for_each_library/wikipedia/test.py
@@ -0,0 +1,1 @@
+import wikipediaprint wikipedia.summary("Wikipedia")print wikipedia.search("Barack")ny = wikipedia.page("New York")print ny.titleprint  ny.urlprint ny.contentprint #wikipedia.set_lang("fr")#print wikipedia.summary("Facebook", sentences=1)
